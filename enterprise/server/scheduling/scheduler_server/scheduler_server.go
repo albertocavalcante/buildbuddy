fe0dae7d16c3aaae3af12b78b5d12b3d66def3f5
Iain Macdonald <xiainx@gmail.com>
2024-02-14 11:45:47 -0800

    Don't delete merging data on unclaimtask

--- enterprise/server/scheduling/scheduler_server/scheduler_server.go
+++ enterprise/server/scheduling/scheduler_server/scheduler_server.go
@@ -1245,8 +1245,6 @@ func (s *SchedulerServer) unclaimTask(ctx context.Context, taskID, leaseID, reco
 	}
 	log.CtxDebugf(ctx, "Released task claim in Redis (reconnecting=%t)", reconnectToken != "")
 
-	action_merger.DeletePendingExecution(ctx, s.rdb, taskID)
-
 	return nil
 }
 
