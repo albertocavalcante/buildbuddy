905258469f89700b802defae3fef5f5433c8cb82
Zoey Greer <zoey@buildbuddy.io>
2024-02-16 12:13:06 -0500

    don't separate READ REPEATABLE or locking reads

--- enterprise/server/githubapp/githubapp.go
+++ enterprise/server/githubapp/githubapp.go
@@ -395,21 +395,22 @@ func (a *GitHubApp) UnlinkGitHubAppInstallation(ctx context.Context, req *ghpb.U
 	if req.GetInstallationId() == 0 {
 		return nil, status.FailedPreconditionError("missing installation_id")
 	}
+	// TODO(zoey): Could make this one query
 	dbh := a.env.GetDBHandle()
-	var ti tables.GitHubAppInstallation
-	err = dbh.NewQuery(ctx, "githubapp_get_installation_for_unlink").Raw(`
-		SELECT *
-		FROM "GitHubAppInstallations"
-		WHERE installation_id = ?
-		`+dbh.SelectForUpdateModifier()+`
-	`, req.GetInstallationId()).Take(&ti)
-	if err != nil {
-		return nil, err
-	}
-	if err := authutil.AuthorizeGroupRole(u, ti.GroupID, role.Admin); err != nil {
-		return nil, err
-	}
 	err = dbh.Transaction(ctx, func(tx interfaces.DB) error {
+		var ti tables.GitHubAppInstallation
+		err := tx.NewQuery(ctx, "githubapp_get_installation_for_unlink").Raw(`
+			SELECT *
+			FROM "GitHubAppInstallations"
+			WHERE installation_id = ?
+			`+dbh.SelectForUpdateModifier()+`
+		`, req.GetInstallationId()).Take(&ti)
+		if err != nil {
+			return err
+		}
+		if err := authutil.AuthorizeGroupRole(u, ti.GroupID, role.Admin); err != nil {
+			return err
+		}
 		return tx.NewQuery(ctx, "githubapp_unlink_installation").Raw(`
 			DELETE FROM "GitHubAppInstallations"
 			WHERE installation_id = ?
